e1a8b1192c00ed25f0b80b8f95902a0135c0996d
Ian Bull <irbull@eclipsesource.com>
2013-05-22 11:05:19 -0700

    Compute the executable path from the artifact and program.
    The path to the executable is now produced from two paramters, the
    artifact and the program. If the artifact is not set, then just the
    program path is used.

--- com.ianbull.p2.touchpoint.exec/src/com/ianbull/p2/touchpoint/exec/Execute.java
+++ com.ianbull.p2.touchpoint.exec/src/com/ianbull/p2/touchpoint/exec/Execute.java
@@ -3,25 +3,53 @@ package com.ianbull.p2.touchpoint.exec;
 import java.io.IOException;
 import java.util.Map;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.equinox.internal.p2.touchpoint.eclipse.EclipseTouchpoint;
+import org.eclipse.equinox.internal.p2.touchpoint.eclipse.Util;
+import org.eclipse.equinox.internal.p2.touchpoint.eclipse.actions.ActionConstants;
 import org.eclipse.equinox.p2.engine.spi.ProvisioningAction;
 
 public class Execute extends ProvisioningAction {
+	
+	public static final String ARTIFACT = "artifact";
+	public static final String PROGRAM = "program";
 
 	public Execute() {
 	}
 
 	@Override
 	public IStatus execute(Map<String, Object> parameters) {
-		String program = (String)parameters.get("program");
+		String artifact = null;
+		if ( parameters.containsKey(ARTIFACT) ) {
+			artifact = (String) parameters.get(ARTIFACT);
+			if (artifact.equals(ActionConstants.PARM_AT_ARTIFACT)) {
+				try {
+					artifact = Util.resolveArtifactParam(parameters);
+				} catch (CoreException e) {
+					return e.getStatus();
+				}
+			}
+		}
+		String program = (String)parameters.get(PROGRAM);
+		String fullPath = computeProgramPath(artifact, program);
 		try {
-			Runtime.getRuntime().exec(program);
+			Runtime.getRuntime().exec(fullPath);
 		} catch (IOException e) {
 			e.printStackTrace();
+			return new Status(IStatus.ERROR, Activator.PLUGIN_ID, "Cannot execute " + fullPath);
 		}
 		return Status.OK_STATUS;
+	}
+		
+	/**
+	 * Computes the path to the program to run
+	 */
+	private String computeProgramPath(String artifact, String programPath) {
+		if ( artifact != null ) {
+			return artifact + "/" + programPath;
+		} 
+		return programPath;
 	}
 
 	@Override
