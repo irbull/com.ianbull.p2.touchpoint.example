2526c77fe6e046f0a649c810dae7e8f30d9ecc54
Ian Bull <irbull@eclipsesource.com>
2013-05-22 11:03:54 -0700

    Added a plug-in ID to the exec activator.

--- com.ianbull.p2.touchpoint.exec/src/com/ianbull/p2/touchpoint/exec/Activator.java
+++ com.ianbull.p2.touchpoint.exec/src/com/ianbull/p2/touchpoint/exec/Activator.java
@@ -5,6 +5,7 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
+	public static final String PLUGIN_ID = "com.ianbull.p2.touchpoint.exec";
 	private static BundleContext context;
 
 	static BundleContext getContext() {
